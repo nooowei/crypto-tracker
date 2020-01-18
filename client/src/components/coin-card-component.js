234426a2655f7907512e6b53043aa3c3f322c261
Wei <wei.xw.xu@gmail.com>
2020-01-18 18:17:49 -0500

    bug fix

--- client/src/components/coin-card-component.js
+++ client/src/components/coin-card-component.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
 // import { Link } from 'react-router-dom';
-import { Container, Typography } from '@material-ui/core';
+import { Container, Typography, ExpansionPanel, ExpansionPanelSummary, ExpansionPanelDetails} from '@material-ui/core';
 import { connect } from 'react-redux';
+import { styled } from '@material-ui/core/styles';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+// import { makeStyles } from '@material-ui/core/styles';
+
+// this is Styled Component API, as opposed to 
+const card = styled(theme => ({
+    root: {
+      width: '100%',
+    },
+    heading: {
+      fontSize: theme.typography.pxToRem(15),
+      fontWeight: theme.typography.fontWeightRegular,
+    },
+}));
 
 // VIEW component - maybe add in addFavourite function later
 class CoinCard extends Component{
-//     // useless constructor so far
-//   constructor(props){
-//     super(props);
-//   }
+    // useless constructor so far
+    constructor(props){
+        super(props);
+    }
 
     getCoinData(){
         let coinName = this.props.coinName;
@@ -16,19 +30,32 @@ class CoinCard extends Component{
         let currentCoin = data[coinName];
         let price = currentCoin.USD.PRICE;
         console.log(price);
-        return price
+        return price;
     }
+    
+    // getClass(){
+    //     const classes = useStyles();
+    //     return classes;
+    // }
 
     render(){
-    return(
-        <Container>
-            <Typography>
-                {this.getCoinData()}
-            </Typography>
-
-
-        </Container>
-    )
+        return(
+            <ExpansionPanel>
+                <ExpansionPanelSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+                >
+                    <Typography>Expansion Panel 1</Typography>
+                </ExpansionPanelSummary>
+                <ExpansionPanelDetails>
+                    <Typography>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+                        sit amet blandit leo lobortis eget.
+                    </Typography>
+                </ExpansionPanelDetails>
+        </ExpansionPanel>
+        )
     }
 
 }
