ab281c8c5bdf3dd65dfebfadac446cd01ce9e2de
Wei <wei.xw.xu@gmail.com>
2020-01-20 23:23:11 -0500

    bug fix

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('client/build'));
 
 // // const uri = process.env.MONGOLAB_URI;
 // const uri = config.get('MONGOLAB_URI');
@@ -31,37 +32,27 @@ app.use(express.static(path.join(__dirname, 'public')));
 //   console.log("MongoDB database connection established successfully");
 // })
 
+// server will serve up index.html in the build folder if in production
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+})
 
-app.use('/', indexRouter);
+// app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
 
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
+// //serve static asset if in production
+// if(process.env.NODE_ENV === 'production'){
+//   console.log("Production environment. Serving static asset from client/build.");
+//   // Set static folder
+//   app.use(express.static('client/build'));
 
-//serve static asset if in production
-if(process.env.NODE_ENV === 'production'){
-  console.log("Production environment. Serving static asset from client/build.");
-  // Set static folder
-  app.use(express.static('client/build'));
-
-  // server will serve up index.html in the build folder if in production
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  })
-}
+//   // server will serve up index.html in the build folder if in production
+//   app.get('/', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//   })
+// }
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
